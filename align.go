2e6d673e8f26bf100cd25d40d00b0c6c4e63bcb2
Liangdy <103975033+liangdy678@users.noreply.github.com>
2024-06-05 09:15:42 +0800

    Create align.go

---
+++ align.go
@@ -0,0 +1,19 @@
+package bioutil
+
+func LeftAlign(ref, alt string) (string, string) {
+	r, a := len(ref), len(alt)
+	var index int
+	for i := 1; true; i++ {
+		if i == r || i == a {
+			index = i - 1
+			break
+		}
+
+		if ref[r-i] != alt[a-i] {
+			index = i - 1
+			break
+		}
+	}
+
+	return ref[0 : r-index], alt[0 : a-index]
+}
